b42f74371fe93b950cc8339f5ff1ca1440d24fa9
Carlo Punzalan <carlo10punzalan@gmail.com>
2019-06-28 17:20:21 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from './services/http-client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'news-and-article-app';
+  posts : any
+  constructor(private httpClientService : HttpClientService){
+  }
+
+  ngOnInit(){
+    this.httpClientService
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe(data=>{
+      console.log(data)
+      this.posts = data
+    })
+  }
+
+  post(){
+    let body = {
+      title:'Angular Indra Training',
+      body:'Angular Indra Training Body'      
+    }
+    this.httpClientService
+    .post('https://jsonplaceholder.typicode.com/posts', body)
+    .subscribe(posts=>{
+      console.log(posts)
+    },
+    (error=>{
+      console.error(error)
+    })
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientService } from './services/http-client.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ArticleComponent } from './shared/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
